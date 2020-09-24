21f53f1db65d9622d6c5d3ba14290a13e17c3322
Kevin March <marchkevin78@gmail.com>
2020-09-23 21:24:20 -0400

    FinishedPersonalInformationExercise

---
+++ BankEncapsulation/PersonalInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BankEncapsulation
+{
+    public class PersonalInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+        private string phoneNumber;
+
+        public string FormattedNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                phoneNumber = Regex.Replace($"{value}", @"(\d{3})(\d{3})(\d{4})", "($1)-$2-$3");
+            }
+        }
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kevinsAccount = new BankAccount();
+           
+            //Console.WriteLine("How much would you like to deposit?");
+
+            //var amountToDeposit = double.Parse(Console.ReadLine());
+
+            //account.Deposit(amountToDeposit);
+
+            //Console.WriteLine($"Thank you! Your balance is now {account.GetBalance()}");
+
+            var kevinsInfo = new PersonalInformation();
+            string number;
+            long result;
+
+            do
+            {
+                Console.WriteLine("What is your phone number starting with the area code?");
+                Console.WriteLine("with the following format: 1234567890");
+                number = Console.ReadLine();
+
+
+            } while (number.Length != 10 || !long.TryParse(number, out result));
+
+            kevinsInfo.FormattedNumber = number;
+            Console.WriteLine(kevinsInfo.FormattedNumber);
+
+            Console.WriteLine($"Your new phone number is {kevinsInfo.FormattedNumber}");
+        }
+    }
+}
